c2bb3e87fcaebd56a5fdec0fd60580af495fbf6d
fabiof2x <fabioar@gmail.com>
2024-01-25 17:23:29 -0300

    Principais métodos do tipo texto - parte 1

---
+++ metodosTipoTexto.py
@@ -0,0 +1,9 @@
+nome = "Fábio"
+sobrenome = "Dias"
+nome_completo = "Fábio Dias"
+
+# Upper: deixa todas as letras em maiusculo
+print(nome.upper())
+
+# Lower: deixa todas as letras em minusculo
+print(nome.lower())
