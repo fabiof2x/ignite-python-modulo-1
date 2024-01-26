41435c1528167cec0901003cd53af541da47cdd8
fabiof2x <fabioar@gmail.com>
2024-01-26 11:43:58 -0300

    Booleanos

---
+++ tipos_variaveis/boleanos.py
@@ -0,0 +1,28 @@
+# Condicionais
+# se condição
+# else = se não
+if True:
+    print("Bloco IF vai ser executado")
+else:
+    print("Bloco ELSE não será executado")
+
+# Operadores lógicos: and e or
+# and    
+if True and True:
+    print("Bloco será executado")
+
+if True and False:
+    print("Bloco não será executado")
+
+if False and False:
+    print("Bloco não será executado")
+
+# or
+if True or False:
+    print("Bloco OR será executado")
+
+if False or False:
+    print("Bloco OR não será executado")
+
+if True or True:
+    print("Bloco OR será executado")
